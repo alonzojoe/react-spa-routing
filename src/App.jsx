fc4fcf3d155a3877473dc166b41fe47e842c90d6
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-05-24 16:00:52 +0800

    setup browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/params",
+    element: <p>URL Params</p>,
+  },
+]);
+
 function App() {
-  return (
-    <>
-      <h3>React SPA</h3>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
